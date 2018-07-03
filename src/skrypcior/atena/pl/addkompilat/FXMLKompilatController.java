4f59abff6406044258803fd3e5dafb70abfb8d0a
Robert Perlejewski <robert.perlejewski@atena.pl>
2018-07-03 07:34:06 +0200

    Udalo sie zapisać nulle

--- src/skrypcior/atena/pl/addkompilat/FXMLKompilatController.java
+++ src/skrypcior/atena/pl/addkompilat/FXMLKompilatController.java
@@ -108,6 +108,8 @@ public class FXMLKompilatController implements Initializable {
             
 
             ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM KOMPILAT");
+            
+            
             while (rs.next()) {
              list.add(new Kompilat(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9)));
             }
@@ -211,7 +213,7 @@ public class FXMLKompilatController implements Initializable {
         preparedStatement.close();
             showInfoAlertBox.showInformationAlertBox("Kompilat dopisano");
         }
-           
+        
     }
 
     @FXML
